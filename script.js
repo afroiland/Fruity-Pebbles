39ecc6bc39969075e058adb9f57d7f5a234f1707
Andrew Froiland <afroiland@Andrews-MacBook-Pro.local>
2016-11-03 11:50:14 -0500

    thursday mid-day commit

--- script.js
+++ script.js
@@ -6,45 +6,102 @@ $(document).ready(function(){
 	var totalAvailableCash = 20;
 
 	var apples = {
-		name:"Apples",
+		name:"apple",
 		price:1
 	};
 
 	var oranges = {
-		name:"Oranges",
+		name:"orange",
 		price:2
 	};
 
 	var bananas = {
-		name:"Bananas",
+		name:"banana",
 		price:3
 	};
 
-	var grapes = {
-		name:"Grapes",
+	var pears = {
+		name:"pear",
 		price:4
 	};
 
-	var fruits = [apples, oranges, bananas, grapes];
+	var fruits = [apples, oranges, bananas, pears];
 	apples.price=5;
-	console.log(fruits);
+	//console.log(fruits);
+
+	function changePrice (fruit){
+		var prChange = randomNumber(-25, 25);
+		fruit.price += prChange/100;
+		//console.log("random number: "+prChange);
+	}
+
+	function intervalChange() {
+
+
+		for (var i = 0; i < fruits.length; i++) {
+			changePrice(fruits[i]);
+			if (fruits[i].price > 9.99) {
+				fruits[i].price = 9.99;
+			} else if ( fruits[i].price < .50) {
+				fruits[i].price = .50;
+			}
+			//console.log(fruits[i].name);
+			var test = (fruits[i].name);
+			//var tmpNum = fruits[i].price;
+			$('#'+test+"Price").text((fruits[i].price).toFixed(2));
+
+			//console.log(test);
+
+		}
+
+		//$('#applePrice').text(2);
+		// $('#oranges').empty();
+		// $('#bananas').empty();
+		// $('#pears').empty();
+
+		//intervalPrice();
+	}
+
+
+	//intervalChange();
+
+	// changePrice(apples);
+	// console.log(apples.price);
 
 	$('h1').append('<h1><span></span>' + totalAvailableCash + '</h1>');
 
 	$('#fruits').append('<div id="apples"></div>');
 	$('#fruits').append('<div id="oranges"></div>');
 	$('#fruits').append('<div id="bananas"></div>');
-	$('#fruits').append('<div id="grapes"></div>');
+	$('#fruits').append('<div id="pears"></div>');
 
 	$('#apples').append('<img src="fruit-pics/apple.jpg" alt="apple">');
 	$('#oranges').append('<img src="fruit-pics/oranges.jpg" alt="orange">');
 	$('#bananas').append('<img src="fruit-pics/bananas.jpg" alt="banana">');
-	$('#grapes').append('<img src="fruit-pics/grapes.png" alt="grapes">');
+	$('#pears').append('<img src="fruit-pics/pears.jpg" alt="pears">');
 
-	$('#fruitPrice').append('<p id="applePrice">Price: '+apples.price+'</p>');
-	$('#fruitPrice').append('<p id="orangePrice">Price: '+oranges.price+'</p>');
-	$('#fruitPrice').append('<p id="bananaPrice">Price: '+bananas.price+'</p>');
-	$('#fruitPrice').append('<p id="grapesPrice">Price: '+grapes.price+'</p>');
+	$('#apples').append('<p id="applePrice">Price: $' + (apples.price).toFixed(2) + '</p>');
+	$('#oranges').append('<p id="orangePrice">Price: $' + (oranges.price).toFixed(2) + '</p>');
+	$('#bananas').append('<p id="bananaPrice">Price: $' + (bananas.price).toFixed(2) + '</p>');
+	$('#pears').append('<p id="pearPrice">Price: $' + (pears.price).toFixed(2) + '</p>');
+
+
+	// $('#applePrice').append('<button id="buyApples">Buy</button>');
+	// $('#orangePrice').append('<button id="buyOranges">Buy</button>');
+	// $('#bananaPrice').append('<button id="buyBananas">Buy</button>');
+	// $('#pearsPrice').append('<button id="buyPears">Buy</button>');
+
+	 setInterval(intervalChange, 1000);
+	 //intervalChange();
+
+	// function intervalPrice() {
+	// 	$('#apples').append('<p id="applePrice">Price: $' + (apples.price).toFixed(2) + '</p>');
+	// 	$('#oranges').append('<p id="orangePrice">Price: $' + (oranges.price).toFixed(2) + '</p>');
+	// 	$('#bananas').append('<p id="bananaPrice">Price: $' + (bananas.price).toFixed(2) + '</p>');
+	// 	$('#pears').append('<p id="pearsPrice">Price: $' + (pears.price).toFixed(2) + '</p>');
+	// }
+
+
 
 
 
